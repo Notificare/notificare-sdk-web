6a9ebda496b4951a6269e11eb77ec96bdc5e6a9d
Helder Pinhal <helder@notifica.re>
2023-04-21 11:13:42 +0100

    refactor(push): cleaner web push registration

--- packages/notificare-push/src/internal/internal-api-web-push.ts
+++ packages/notificare-push/src/internal/internal-api-web-push.ts
@@ -5,7 +5,7 @@ import {
   NotificareApplication,
   NotificareInternalOptions,
 } from '@notificare/core';
-import { arrayBufferToBase64, arrayBufferToBase64Url, base64UrlToUint8Array } from './utils';
+import { arrayBufferToBase64 } from './utils';
 import { logger } from '../logger';
 import { logNotificationInfluenced, logNotificationReceived } from './internal-api-events';
 import {
@@ -15,6 +15,7 @@ import {
   notifySystemNotificationReceived,
   notifyUnknownNotificationReceived,
 } from './consumer-events';
+import { createWebPushSubscription, registerServiceWorker } from './web-push/service-worker';
 
 export function hasWebPushSupport(): boolean {
   // The navigator.standalone check ensures that iOS Safari with WebPush
@@ -39,84 +40,10 @@ export async function enableWebPushNotifications(
     );
   }
 
-  const serviceWorkerLocation = options.serviceWorker;
-  if (!serviceWorkerLocation) {
-    throw new Error(
-      'Missing service worker location. Please check your configurations (config.json).',
-    );
-  }
+  const registration = await registerServiceWorker(options);
+  const subscription = await createWebPushSubscription(registration, publicKey);
 
-  const serviceWorkerAllowed =
-    options.applicationHost.includes('localhost') || options.applicationHost.startsWith('https');
-
-  if (!serviceWorkerAllowed) {
-    throw new Error('Service workers are only available over HTTPS or localhost.');
-  }
-
-  let registrationOptions: RegistrationOptions | undefined;
-  if (options.serviceWorkerScope) {
-    registrationOptions = {
-      scope: options.serviceWorkerScope,
-    };
-  }
-
-  await registerServiceWorker(serviceWorkerLocation, registrationOptions);
-
-  const registration = await navigator.serviceWorker.ready;
-  const existingSubscription = await registration.pushManager.getSubscription();
-
-  if (
-    !existingSubscription ||
-    !existingSubscription.options.applicationServerKey ||
-    arrayBufferToBase64Url(existingSubscription.options.applicationServerKey) !==
-      application.websitePushConfig.vapid.publicKey
-  ) {
-    logger.debug('Subscribing for push notifications.');
-
-    try {
-      const subscription = await registration.pushManager.subscribe({
-        // name: 'push',
-        userVisibleOnly: true,
-        applicationServerKey: base64UrlToUint8Array(application.websitePushConfig.vapid.publicKey),
-      });
-
-      return getPushTokenFromPushSubscription(subscription);
-    } catch (e) {
-      if (
-        existingSubscription &&
-        existingSubscription.options.applicationServerKey &&
-        arrayBufferToBase64Url(existingSubscription.options.applicationServerKey) !==
-          application.websitePushConfig.vapid.publicKey
-      ) {
-        logger.info('Removing stale push subscription.');
-        await existingSubscription.unsubscribe();
-
-        throw new Error(
-          'Subscription for service worker is no longer valid and has been unsubscribed. Please register again.',
-        );
-      }
-
-      logger.error('Failed to subscribe to push notifications.', e);
-      throw e;
-    }
-  }
-
-  // If there's an expiration date, renew subscription 5 days before the end.
-  if (
-    existingSubscription.expirationTime &&
-    Date.now() > existingSubscription.expirationTime - 432000000
-  ) {
-    logger.debug('Renewing existing push subscription.');
-
-    await existingSubscription.unsubscribe();
-    await registration.pushManager.subscribe({
-      // name: 'push',
-      userVisibleOnly: true,
-      applicationServerKey: base64UrlToUint8Array(application.websitePushConfig.vapid.publicKey),
-    });
-  }
-
-  return getPushTokenFromPushSubscription(existingSubscription);
+  return getPushTokenFromPushSubscription(subscription);
 }
 
 export async function disableWebPushNotifications() {
@@ -183,25 +110,6 @@ export interface PushToken {
 export interface PushKeys {
   p256dh: string;
   auth: string;
-}
-
-async function registerServiceWorker(
-  serviceWorkerLocation: string,
-  options: RegistrationOptions | undefined,
-) {
-  const registrations = await navigator.serviceWorker.getRegistrations();
-
-  const activeRegistration = registrations.find((r) =>
-    r.active?.scriptURL?.includes(serviceWorkerLocation),
-  );
-
-  if (activeRegistration) {
-    logger.warning('Using previously registered service worker.');
-    return;
-  }
-
-  logger.debug('Registering new service worker.');
-  await navigator.serviceWorker.register(serviceWorkerLocation, options);
 }
 
 function getPushTokenFromPushSubscription(subscription: PushSubscription): PushToken {
---
+++ packages/notificare-push/src/internal/web-push/service-worker.ts
@@ -0,0 +1,106 @@
+import { NotificareInternalOptions } from '@notificare/core';
+import { logger } from '../../logger';
+import { arrayBufferToBase64Url, base64UrlToUint8Array } from '../utils';
+
+export async function registerServiceWorker(
+  options: NotificareInternalOptions,
+): Promise<ServiceWorkerRegistration> {
+  const workerLocation = getWorkerLocation(options);
+  if (!workerLocation) {
+    throw new Error(
+      'Missing service worker location. Please check your configurations (config.json).',
+    );
+  }
+
+  if (!hasSupportedProtocol(options.applicationHost)) {
+    throw new Error('Service workers are only available over HTTPS or localhost.');
+  }
+
+  const activeRegistration = await getActiveWorkerRegistration(workerLocation);
+  if (activeRegistration) {
+    logger.debug('Using previously registered service worker.');
+    return navigator.serviceWorker.ready;
+  }
+
+  logger.debug('Registering new service worker.');
+
+  let registrationOptions: RegistrationOptions | undefined;
+  if (options.serviceWorkerScope) {
+    registrationOptions = {
+      scope: options.serviceWorkerScope,
+    };
+  }
+
+  await navigator.serviceWorker.register(workerLocation, registrationOptions);
+  return navigator.serviceWorker.ready;
+}
+
+export async function createWebPushSubscription(
+  registration: ServiceWorkerRegistration,
+  vapidPublicKey: string,
+): Promise<PushSubscription> {
+  //
+  const currentSubscription = await registration.pushManager.getSubscription();
+
+  if (currentSubscription) {
+    const currentApplicationServerKey =
+      currentSubscription.options.applicationServerKey &&
+      arrayBufferToBase64Url(currentSubscription.options.applicationServerKey);
+
+    if (currentApplicationServerKey !== vapidPublicKey) {
+      logger.warning('The VAPID public keys changed.');
+
+      logger.debug('Removing stable push subscription.');
+      await currentSubscription.unsubscribe();
+
+      logger.debug('Subscribing for push notifications again.');
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: base64UrlToUint8Array(vapidPublicKey),
+      });
+    }
+
+    if (subscriptionAboutToExpire(currentSubscription)) {
+      logger.debug('Renewing existing push subscription.');
+      await currentSubscription.unsubscribe();
+
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: base64UrlToUint8Array(vapidPublicKey),
+      });
+    }
+
+    logger.debug('Using the current push subscription.');
+    return currentSubscription;
+  }
+
+  logger.debug('Subscribing for push notifications.');
+  return registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: base64UrlToUint8Array(vapidPublicKey),
+  });
+}
+
+function hasSupportedProtocol(applicationHost: string): boolean {
+  return applicationHost.includes('localhost') || applicationHost.startsWith('https');
+}
+
+function getWorkerLocation(options: NotificareInternalOptions): string | undefined {
+  if (!options.serviceWorker) return undefined;
+
+  return options.serviceWorker;
+}
+
+async function getActiveWorkerRegistration(
+  workerLocation: string,
+): Promise<ServiceWorkerRegistration | undefined> {
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  return registrations.find((r) => r.active?.scriptURL?.includes(workerLocation));
+}
+
+function subscriptionAboutToExpire(subscription: PushSubscription): boolean {
+  const { expirationTime } = subscription;
+  if (!expirationTime) return false;
+
+  return Date.now() > expirationTime - 432000000;
+}
