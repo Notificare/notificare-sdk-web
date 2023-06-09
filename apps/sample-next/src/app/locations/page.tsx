60360f180d24905a58b8e6805a44bb3d0a0225c3
Helder Pinhal <helder@notifica.re>
2023-06-09 12:34:08 +0200

    feat(sample): add geo sample

--- apps/sample-next/src/app/locations/page.tsx
+++ apps/sample-next/src/app/locations/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { useNotificare } from "@/context/notificare";
+import { disableLocationUpdates, enableLocationUpdates } from "@notificare/geo";
+
 export default function Locations() {
+  const notificareState = useNotificare();
+
   return (
     <>
-      <p>Locations</p>
+      {notificareState !== "launched" ? (
+        <p>stuff is not ready</p>
+      ) : (
+        <>
+          <div className="space-x-4">
+            <button
+              type="button"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => enableLocationUpdates()}
+            >
+              Enable location updates
+            </button>
+
+            <button
+              type="button"
+              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-indigo-600 hover:shadow-sm hover:bg-indigo-100"
+              onClick={() => disableLocationUpdates()}
+            >
+              Disable location updates
+            </button>
+          </div>
+        </>
+      )}
     </>
   );
 }
