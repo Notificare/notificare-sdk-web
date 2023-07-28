247e31723348ec8c898f5f1ca0c30110d60d6e2a
Helder Pinhal <helder@notifica.re>
2023-07-28 09:37:32 +0100

    feat(core): add reply & webhook requests

--- packages/notificare-core/src/public-api.ts
+++ packages/notificare-core/src/public-api.ts
@@ -17,16 +17,20 @@ import {
 import { NotificareApplication } from './models/notificare-application';
 import { NotificareNotConfiguredError } from './errors/notificare-not-configured-error';
 import { components } from './internal/component-cache';
-import { NotificareNotification } from './models/notificare-notification';
+import {
+  NotificareNotification,
+  NotificareNotificationAction,
+} from './models/notificare-notification';
 import {
   convertNetworkNotificationToPublic,
   NetworkNotificationResponse,
 } from './internal/network/responses/notification-response';
 import { notifyOnReady, notifyUnlaunched } from './internal/consumer-events';
 import { NotificareNotReadyError } from './errors/notificare-not-ready-error';
-import { clearTags } from './public-api-device';
+import { clearTags, getCurrentDevice } from './public-api-device';
 import { SDK_VERSION as SDK_VERSION_INTERNAL } from './internal/version';
 import { deleteDevice, registerTemporaryDevice } from './internal/internal-api-device';
+import { NotificareDeviceUnavailableError } from './errors/notificare-device-unavailable-error';
 
 export const SDK_VERSION: string = SDK_VERSION_INTERNAL;
 
@@ -236,6 +240,86 @@ export async function fetchNotification(id: string): Promise<NotificareNotificat
 
   const { notification }: NetworkNotificationResponse = await response.json();
   return convertNetworkNotificationToPublic(notification);
+}
+
+export async function createNotificationReply(
+  notification: NotificareNotification,
+  action: NotificareNotificationAction,
+  data?: NotificationReplyData,
+): Promise<void> {
+  if (!isConfigured()) throw new NotificareNotConfiguredError();
+
+  const options = getOptions();
+  if (!options) throw new NotificareNotConfiguredError();
+
+  const device = getCurrentDevice();
+  if (!device) throw new NotificareDeviceUnavailableError();
+
+  let mediaUrl: string | undefined;
+
+  if (data?.media && data?.mimeType) {
+    const formData = new FormData();
+    formData.append('file', data.media);
+
+    const response = await request(`/api/upload/reply`, {
+      method: 'POST',
+      formData,
+    });
+
+    const { filename } = await response.json();
+    mediaUrl = `${options.services.awsStorageHost}${filename}`;
+  }
+
+  await request('/api/reply', {
+    method: 'POST',
+    body: {
+      notification: notification.id,
+      deviceID: device.id,
+      userID: device.userId,
+      label: action.label,
+      data: {
+        target: action.target,
+        message: data?.message,
+        media: mediaUrl,
+        mimeType: data?.mimeType,
+      },
+    },
+  });
+}
+
+export interface NotificationReplyData {
+  message?: string;
+  media?: Blob;
+  mimeType?: string;
+}
+
+export async function callNotificationWebhook(
+  notification: NotificareNotification,
+  action: NotificareNotificationAction,
+): Promise<void> {
+  if (!action.target) throw new Error('Unable to execute webhook without a target for the action.');
+
+  const url = new URL(action.target);
+
+  const device = getCurrentDevice();
+  const data: Record<string, string> = {};
+
+  // Populate the data with the target's query parameters.
+  url.searchParams.forEach((key) => {
+    const value = url.searchParams.get(key);
+    if (value) data[key] = value;
+  });
+
+  data.target = url.origin;
+  data.label = action.label;
+  data.notificationID = notification.id;
+  if (device?.id) data.deviceID = device.id;
+  if (device?.userId) data.userID = device.userId;
+
+  await request('/api/reply/webhook', {
+    method: 'POST',
+    body: data,
+  });
 }
 
 function printLaunchSummary(application: NotificareApplication) {
