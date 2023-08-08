8b608b17958c5691824c5ca67517de352122d014
Helder Pinhal <helder@notifica.re>
2023-08-08 16:51:52 +0100

    feat(push): auto enable remote notifications

--- packages/notificare-push/src/public-api.ts
+++ packages/notificare-push/src/public-api.ts
@@ -1,30 +1,16 @@
 import {
   getApplication,
-  getCurrentDevice,
-  getOptions,
-  isConfigured,
   isReady,
   NotificareApplicationUnavailableError,
-  NotificareDeviceUnavailableError,
-  NotificareNotConfiguredError,
   NotificareNotReadyError,
   NotificareServiceUnavailableError,
-  registerPushDevice,
-  registerTemporaryDevice,
-  request,
 } from '@notificare/core';
 import { logger } from './logger';
 import {
-  disableWebPushNotifications,
-  enableWebPushNotifications,
-  hasWebPushSupport,
-  postMessageToServiceWorker,
-} from './internal/internal-api-web-push';
-import {
-  enableSafariPushNotifications,
-  hasSafariPushSupport,
-} from './internal/internal-api-safari-push';
-import { logPushRegistration } from './internal/internal-api-events';
+  disableRemoteNotifications as disableRemoteNotificationsInternal,
+  enableRemoteNotifications as enableRemoteNotificationsInternal,
+} from './internal/internal-api';
+import { getRemoteNotificationsEnabled } from './internal/storage/local-storage';
 
 export {
   onNotificationReceived,
@@ -34,78 +20,20 @@ export {
   onUnknownNotificationReceived,
 } from './internal/consumer-events';
 
-export function hasWebPushCapabilities(): boolean {
-  return hasWebPushSupport() || hasSafariPushSupport();
+export { hasWebPushCapabilities } from './internal/internal-api';
+
+export function hasRemoteNotificationsEnabled(): boolean {
+  return getRemoteNotificationsEnabled();
 }
 
 export async function enableRemoteNotifications(): Promise<void> {
   checkPrerequisites();
-
-  const options = getOptions();
-  if (!options) throw new NotificareNotConfiguredError();
-
-  const application = getApplication();
-  if (!application) throw new NotificareNotReadyError();
-
-  if (!application.websitePushConfig?.icon) {
-    throw new Error(
-      'Missing application icon. Please check your Website Push configurations in our dashboard before proceeding.',
-    );
-  }
-
-  if (application.websitePushConfig.allowedDomains.indexOf(options.applicationHost) === -1) {
-    throw new Error(
-      'Missing allowed domain. Please check your Website Push configurations in our dashboard before proceeding.',
-    );
-  }
-
-  if (!hasWebPushSupport() && !hasSafariPushSupport()) {
-    throw new Error('Your browser does not support Service Workers nor Safari Website Push.');
-  }
-
-  if (hasWebPushSupport()) {
-    const token = await enableWebPushNotifications(application, options);
-
-    await registerPushDevice({
-      transport: 'WebPush',
-      token: token.endpoint,
-      keys: token.keys,
-    });
-
-    try {
-      await postMessageToServiceWorker({
-        action: 're.notifica.ready',
-      });
-    } catch (e) {
-      logger.warning('Failed to send a message to the service worker.', e);
-    }
-  } else if (hasSafariPushSupport()) {
-    const token = await enableSafariPushNotifications();
-
-    await registerPushDevice({
-      transport: 'WebsitePush',
-      token,
-    });
-  }
-
-  try {
-    await updateNotificationSettings();
-  } catch (e) {
-    logger.warning('Unable to update the notification settings.', e);
-  }
+  await enableRemoteNotificationsInternal();
 }
 
 export async function disableRemoteNotifications(): Promise<void> {
   checkPrerequisites();
-
-  const device = getCurrentDevice();
-  if (!device) throw new NotificareDeviceUnavailableError();
-
-  if (device.transport === 'WebPush') {
-    await disableWebPushNotifications();
-  }
-
-  await registerTemporaryDevice();
+  await disableRemoteNotificationsInternal();
 }
 
 function checkPrerequisites() {
@@ -123,30 +51,5 @@ function checkPrerequisites() {
   if (!application.services.websitePush) {
     logger.warning('Notificare website push functionality is not enabled.');
     throw new NotificareServiceUnavailableError('websitePush');
-  }
-}
-
-async function updateNotificationSettings() {
-  if (!isConfigured()) throw new NotificareNotConfiguredError();
-
-  const device = getCurrentDevice();
-  if (!device) throw new NotificareDeviceUnavailableError();
-
-  const allowedUI = device.transport !== 'Notificare' && hasWebPushCapabilities();
-
-  await request(`/api/device/${encodeURIComponent(device.id)}`, {
-    method: 'PUT',
-    body: {
-      allowedUI,
-      webPushCapable: hasWebPushCapabilities(),
-    },
-  });
-
-  const firstRegistrationStr = localStorage.getItem('re.notifica.push.first_registration');
-  const firstRegistration = firstRegistrationStr == null || firstRegistrationStr === 'true';
-
-  if (allowedUI && firstRegistration) {
-    await logPushRegistration();
-    localStorage.setItem('re.notifica.push.first_registration', 'false');
   }
 }
