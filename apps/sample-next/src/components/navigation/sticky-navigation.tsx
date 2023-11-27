c154fa758fcec44a19ea65219c658d52314601f1
Helder Pinhal <helder@notifica.re>
2023-11-27 19:18:38 +0000

    feat(sample): show registered user avatar

--- apps/sample-next/src/components/navigation/sticky-navigation.tsx
+++ apps/sample-next/src/components/navigation/sticky-navigation.tsx
@@ -3,10 +3,17 @@
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { useNavigation } from "@/context/navigation";
 import { InboxBell } from "@/components/navigation/inbox-bell";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import { getCurrentDevice, NotificareDevice } from "notificare-web/core";
+import { Gravatar } from "@/components/gravatar";
+import { useOnDeviceRegistered } from "@/notificare/hooks/events/core/device-registered";
 
 export function StickyNavigation() {
   const { sidebar } = useNavigation();
+  const [device, setDevice] = useState<NotificareDevice>();
+
+  useEffect(() => setDevice(getCurrentDevice()), []);
+  useOnDeviceRegistered((device) => setDevice(device));
 
   return (
     <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
@@ -30,41 +37,23 @@ export function StickyNavigation() {
           <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200" aria-hidden="true" />
 
           <div className="-m-1.5 flex items-center p-1.5">
-            <Image
-              className="h-8 w-8 rounded-full bg-gray-50"
-              src="https://www.gravatar.com/avatar/1a73f51bd2d8f8114835508ecd678c66?s=128&d=https://dashboard.notifica.re/assets/images/no-gravatar-blue.png"
-              alt="Avatar"
-              width={32}
-              height={32}
-            />
+            <Gravatar email={device?.userId ?? ""} />
             <span className="hidden lg:flex lg:items-center">
               <span
                 className="ml-4 text-sm font-semibold leading-6 text-gray-900"
                 aria-hidden="true"
               >
-                Helder Pinhal
+                {device?.userName ?? "Anonymous"}
               </span>
             </span>
           </div>
         </div>
-
-        {/*<form className="relative flex flex-1" action="#" method="GET">*/}
-        {/*  <label htmlFor="search-field" className="sr-only">*/}
-        {/*    Search*/}
-        {/*  </label>*/}
-        {/*  <MagnifyingGlassIcon*/}
-        {/*    className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"*/}
-        {/*    aria-hidden="true"*/}
-        {/*  />*/}
-        {/*  <input*/}
-        {/*    id="search-field"*/}
-        {/*    className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"*/}
-        {/*    placeholder="Search..."*/}
-        {/*    type="search"*/}
-        {/*    name="search"*/}
-        {/*  />*/}
-        {/*</form>*/}
       </div>
     </div>
   );
 }
+
+type UserInformation = {
+  email: string;
+  name: string;
+};
