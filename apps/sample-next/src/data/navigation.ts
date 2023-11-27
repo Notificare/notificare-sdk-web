5daca9a4a9da865ef2cdf9711fb37d21b4a8c21e
Helder Pinhal <helder@notifica.re>
2023-11-27 13:51:27 +0000

    feat(sample): add application page to the navigation

--- apps/sample-next/src/data/navigation.ts
+++ apps/sample-next/src/data/navigation.ts
@@ -5,6 +5,7 @@ import {
   Cog6ToothIcon,
   ComputerDesktopIcon,
   HomeIcon,
+  IdentificationIcon,
   InboxIcon,
   TagIcon,
 } from "@heroicons/react/24/outline";
@@ -19,6 +20,7 @@ export interface SideBarNavigationItem {
 
 export const sideBarNavigationItems: SideBarNavigationItem[] = [
   { label: "Home", href: "/", icon: HomeIcon },
+  { label: "Application", href: "/application", icon: IdentificationIcon },
   { label: "Device", href: "/device", icon: ComputerDesktopIcon },
   { label: "Inbox", href: "/inbox", icon: InboxIcon },
   { label: "Segmentation", href: "/segmentation", icon: TagIcon },
