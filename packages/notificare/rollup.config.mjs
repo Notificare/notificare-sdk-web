cfdab61d63437f0db9ffe4920fc87c3564b4fcbb
Helder Pinhal <helder@notifica.re>
2023-12-10 16:59:10 +0000

    chore: fix import order lint

--- packages/notificare/rollup.config.mjs
+++ packages/notificare/rollup.config.mjs
@@ -1,5 +1,5 @@
 import typescript from 'typescript';
-import { build } from '../../scripts/rollup/rollup.umbrella.config.mjs';
 import pkg from './package.json' assert { type: 'json' };
+import { build } from '../../scripts/rollup/rollup.umbrella.config.mjs';
 
 export default build(pkg, { typescript, rootDirectory: import.meta.url });
