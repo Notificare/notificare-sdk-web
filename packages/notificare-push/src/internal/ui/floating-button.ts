de7711f9ca006c079b34e1c2b42810b9a031d6fc
Helder Pinhal <helder@notifica.re>
2024-05-14 11:47:40 +0100

    refactor(push): dry onboarding identifier

--- packages/notificare-push/src/internal/ui/floating-button.ts
+++ packages/notificare-push/src/internal/ui/floating-button.ts
@@ -3,11 +3,11 @@ import {
   NotificareWebsitePushConfigLaunchConfigFloatingButtonOptions,
   NotificareWebsitePushConfigLaunchConfigFloatingButtonVerticalAlignment,
 } from '@notificare/web-core';
-import { createRoot } from '@notificare/web-ui';
 import createDeniedBellIcon from '../../assets/bell-icon-denied.svg';
 import createGrantedBellIcon from '../../assets/bell-icon-granted.svg';
 import createBellIcon from '../../assets/bell-icon.svg';
 import { getPushPermissionStatus } from '../../utils/push';
+import { createRootElement, removeRootElement } from './base';
 
 let permissionCheckTimer: number | undefined;
 
@@ -17,7 +17,7 @@ export function showFloatingButton({
 }: ShowFloatingButtonOptions) {
   ensureCleanState();
 
-  const root = createRoot('notificare-push');
+  const root = createRootElement();
   root.classList.add('notificare-push-floating-button');
 
   const floatingButton = root.appendChild(document.createElement('div'));
@@ -58,8 +58,7 @@ export interface ShowFloatingButtonOptions {
 }
 
 function ensureCleanState() {
-  const root = document.getElementById('notificare-push');
-  if (root) root.remove();
+  removeRootElement();
 
   const timer = permissionCheckTimer;
   if (timer) window.clearInterval(timer);
