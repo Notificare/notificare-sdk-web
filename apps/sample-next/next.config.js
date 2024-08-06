74c60240431f78001736b50a042da804e900c84d
Helder Pinhal <helder@notifica.re>
2024-08-06 16:04:26 +0100

    refactor(sample): replace withPWA() config with symlink for the service worker

--- apps/sample-next/next.config.js
+++ apps/sample-next/next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
-
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  register: false,
-});
-
-module.exports = withPWA(nextConfig);
---
+++ apps/sample-next/public/sw.js
@@ -0,0 +1,1 @@
+../../../node_modules/notificare-web/push/sw/dist/index.mjs
