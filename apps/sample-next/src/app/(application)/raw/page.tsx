6557e4a6f153c25e2adebb286b06b7e5d62c1074
Helder Pinhal <helder@notifica.re>
2025-05-21 17:02:45 +0100

    fix(sample-app): fix raw page static render

--- apps/sample-next/src/app/(application)/raw/page.tsx
+++ apps/sample-next/src/app/(application)/raw/page.tsx
@@ -1,35 +1,43 @@
 "use client";
 
+import { Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { PageHeader } from "@/components/page-header";
 
-export default function Raw() {
+export default function RawPage() {
+  return (
+    <>
+      <PageHeader title="Raw URL" message="Inspect the URL and its search parameters." />
+      <Suspense>
+        <RawPageContent />
+      </Suspense>
+    </>
+  );
+}
+
+function RawPageContent() {
   const path = usePathname();
   const searchParams = useSearchParams();
 
   return (
-    <>
-      <PageHeader title="Raw URL" message="Inspect the URL and its search parameters." />
-
-      <div className="flex flex-col gap-8">
-        <div>
-          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">URL</p>
-          <p className="text-sm text-gray-500 truncate dark:text-gray-200">{path}</p>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-            Search parameters
-          </p>
-
-          {[...searchParams].map(([key, value]) => (
-            <div key={key}>
-              <p className="text-sm font-medium text-gray-500 truncate dark:text-gray-200">{key}</p>
-              <p className="text-sm text-gray-500 truncate dark:text-gray-200">{value}</p>
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col gap-8">
+      <div>
+        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">URL</p>
+        <p className="text-sm text-gray-500 truncate dark:text-gray-200">{path}</p>
       </div>
-    </>
+
+      <div className="flex flex-col gap-2">
+        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+          Search parameters
+        </p>
+
+        {[...searchParams].map(([key, value]) => (
+          <div key={key}>
+            <p className="text-sm font-medium text-gray-500 truncate dark:text-gray-200">{key}</p>
+            <p className="text-sm text-gray-500 truncate dark:text-gray-200">{value}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
