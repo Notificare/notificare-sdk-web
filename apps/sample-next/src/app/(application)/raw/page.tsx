e18d821ec9bd821025dff4e7e0ba62d13162990c
Helder Pinhal <helder@notifica.re>
2025-05-21 17:45:37 +0100

    feat(sample-app): output full url

--- apps/sample-next/src/app/(application)/raw/page.tsx
+++ apps/sample-next/src/app/(application)/raw/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { PageHeader } from "@/components/page-header";
 
@@ -19,11 +19,17 @@ function RawPageContent() {
   const path = usePathname();
   const searchParams = useSearchParams();
 
+  const [href, setHref] = useState<string>();
+  useEffect(() => {
+    setHref(window.location.href);
+  }, []);
+
   return (
     <div className="flex flex-col gap-8">
       <div>
         <p className="text-sm font-medium text-gray-900 truncate dark:text-white">URL</p>
         <p className="text-sm text-gray-500 truncate dark:text-gray-200">{path}</p>
+        <p className="text-sm text-gray-500 truncate dark:text-gray-200">{href}</p>
       </div>
 
       <div className="flex flex-col gap-2">
